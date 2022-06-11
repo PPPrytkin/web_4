c135476e364b09fd0c45dfd1da641a0d4441931a
Pavel Prytkin <p.prytkin@mail.ru>
2022-06-11 15:49:25 +0300

    task 2

--- Node/text-files/index.js
+++ Node/text-files/index.js
@@ -1,41 +1,40 @@
 const fs = require("fs").promises;
+const path = require("path");
 
 async function findSalesFiles(folderName) {
-  // массив будет содержать файлы продаж по мере их обнаружения 
-  let salesFiles = [];
+    // массив будет содержать файлы продаж по мере их обнаружения
+    let salesFiles = [];
 
-  async function findFiles(folderName) {
-    // чтение всех элементов в текущей папке
-    const items = await fs.readdir(folderName, { withFileTypes: true });
-
-    // перебор каждого найденного элемента
-    for (item of items) {
-      // если элемент является каталогом, в нем нужно будет искать файлы
-      if (item.isDirectory()) {
-        // рекурсивно искать в этом каталоге файлы
-        await findFiles(`${folderName}\\${item.name}`);
-      }
-        // Убедитесь, что обнаруженный файл является файлом sales.json
-      if (item.name === "sales.json") {
-        // сохранить путь к файлу в массиве salesFiles
-        salesFiles.push(`${folderName}\\${item.name}`);
-        //if (salesFiles.length === 4) {
-          //console.log(salesFiles);
-          ///return salesFiles;
+    async function findFiles(folderName) {
+        // чтение всех элементов в текущей папке
+        const items = await fs.readdir(folderName, { withFileTypes: true });
+        // перебор каждого найденного элемента
+        for (item of items) {
+            // если элемент является каталогом, в нем нужно будет искать файлы
+            // рекурсивно искать в этом каталоге файлы
+            if (item.isDirectory()) {
+                // если это папка, то этот метод вызывается снова и передается
+                // рекурсивно сформированный путь к каталогу для поиска
+                await findFiles(path.join(folderName, item.name));
+            }
+            // Убедитесь, что обнаруженный файл является файлом sales.json
+            if (path.extname(item.name) === ".json") {
+                await salesFiles.push(path.join(folderName, item.name));
+            }
         }
-      }
-    
     }
 
-  // найти файлы продаж
-  await findFiles(folderName);
+    // найти файлы продаж
+    await findFiles(folderName);
 
-  return salesFiles;
+    // вернуть массив найденных путей к файлам
+    return salesFiles;
 }
 
 async function main() {
-  const salesFiles = await findSalesFiles("stores");
-  console.log(salesFiles);
+    const salesDir = path.join(__dirname, "stores");
+    const salesFiles = await findSalesFiles(salesDir);
+    console.log(salesFiles);
 }
 
-main();
+main()
